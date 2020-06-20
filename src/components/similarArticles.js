3c966b151c87e7ae9229b37a7f738b2261d0805e
Takako Sekiya <takako.ballet.1216@gmail.com>
2020-06-20 17:04:34 +0800

    added frontend

---
+++ src/components/similarArticles.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { graphql } from "gatsby"
+import ReactMarkdown from "react-markdown"
+import Moment from "react-moment"
+import Layout from "../components/layout"
+import Img from "gatsby-image"
+import {Link} from "gatsby"
