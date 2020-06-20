5db704599de9d21acfc852ccc6dc12af3869a170
Takako Sekiya <takako.ballet.1216@gmail.com>
2020-06-20 23:39:44 +0800

    modified

--- src/components/similarArticles.js
+++ src/components/similarArticles.js
@@ -1,7 +1,1 @@
-import React from "react"
-import { graphql } from "gatsby"
-import ReactMarkdown from "react-markdown"
-import Moment from "react-moment"
-import Layout from "../components/layout"
-import Img from "gatsby-image"
-import {Link} from "gatsby"
+
